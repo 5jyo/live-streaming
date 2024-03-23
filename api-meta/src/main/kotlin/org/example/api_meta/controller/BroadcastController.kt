c628cdc4fcc33418c9ae08124c707194a1d67245
jokingmoony <penubokim@gmail.com>
2024-03-23 22:11:43 +0900

    Add get broadcast API

--- api-meta/src/main/kotlin/org/example/api_meta/controller/BroadcastController.kt
+++ api-meta/src/main/kotlin/org/example/api_meta/controller/BroadcastController.kt
@@ -2,16 +2,25 @@ package org.example.api_meta.controller
 
 import org.example.api_meta.dto.Broadcast
 import org.example.api_meta.service.BroadcastService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class BroadcastController(
-        private val broadcastService: BroadcastService
+    private val broadcastService: BroadcastService
 ) {
     @GetMapping("/broadcasts")
     fun list(): List<Broadcast> {
         return broadcastService.getList()
+    }
+
+    @GetMapping("/broadcasts/{broadcast_id}")
+    fun get(@PathVariable("broadcast_id") broadcastId: Long): ResponseEntity<Broadcast> {
+        val broadcast = broadcastService.get(broadcastId)
+            ?: return ResponseEntity.notFound().build()
+        return ResponseEntity.ok(broadcast)
     }
 
 }
--- api-meta/src/main/kotlin/org/example/api_meta/service/BroadcastService.kt
+++ api-meta/src/main/kotlin/org/example/api_meta/service/BroadcastService.kt
@@ -2,6 +2,7 @@ package org.example.api_meta.service
 
 import org.example.api_meta.dto.Broadcast
 import org.example.api_meta.repository.BroadcastRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,5 +11,9 @@ class BroadcastService(
 ) {
     fun getList(): List<Broadcast> {
         return broadcastRepository.findAll()
+    }
+
+    fun get(broadcastId: Long): Broadcast? {
+        return broadcastRepository.findByIdOrNull(broadcastId)
     }
 }
