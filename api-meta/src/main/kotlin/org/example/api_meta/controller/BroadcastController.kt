88b9107afe76cc540add62288fc5bf2dcc92b282
jokingmoony <penubokim@gmail.com>
2024-03-24 12:19:44 +0900

    Create POST broadcast API

--- api-meta/src/main/kotlin/org/example/api_meta/controller/BroadcastController.kt
+++ api-meta/src/main/kotlin/org/example/api_meta/controller/BroadcastController.kt
@@ -1,10 +1,14 @@
 package org.example.api_meta.controller
 
 import org.example.api_meta.dto.Broadcast
+import org.example.api_meta.dto.BroadcastForm
 import org.example.api_meta.service.BroadcastService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -21,6 +25,13 @@ class BroadcastController(
         val broadcast = broadcastService.get(broadcastId)
             ?: return ResponseEntity.notFound().build()
         return ResponseEntity.ok(broadcast)
+    }
+
+    @PostMapping("/broadcasts")
+    fun post(@RequestBody broadcastForm : BroadcastForm): ResponseEntity<Broadcast> {
+        val broadcast = broadcastService.create(broadcastForm)
+            ?: return ResponseEntity.badRequest().build()
+        return ResponseEntity(broadcast, HttpStatus.CREATED)
     }
 
 }
--- api-meta/src/main/kotlin/org/example/api_meta/service/BroadcastService.kt
+++ api-meta/src/main/kotlin/org/example/api_meta/service/BroadcastService.kt
@@ -1,6 +1,7 @@
 package org.example.api_meta.service
 
 import org.example.api_meta.dto.Broadcast
+import org.example.api_meta.dto.BroadcastForm
 import org.example.api_meta.repository.BroadcastRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -16,4 +17,13 @@ class BroadcastService(
     fun get(broadcastId: Long): Broadcast? {
         return broadcastRepository.findByIdOrNull(broadcastId)
     }
+
+    fun create(broadcastForm: BroadcastForm): Broadcast? {
+        val broadcast = Broadcast(
+            name = broadcastForm.name,
+            description = broadcastForm.description)
+        return broadcastRepository.save(broadcast)
+    }
 }
+
+
