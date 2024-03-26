18a3ac039668d4d4e43bad87a61dd0a724967bc5
junsu.shin <junsu.shin@linecorp.com>
2024-03-26 21:57:28 +0900

    Enable https for meta server

---
+++ api-meta/src/main/kotlin/org/example/api_meta/config/WebConfig.kt
@@ -0,0 +1,12 @@
+package org.example.api_meta.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedOrigins("*")
+    }
+}
