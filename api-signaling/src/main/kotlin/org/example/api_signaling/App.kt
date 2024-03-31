f0e596ad50736ecdfa0c9d9c5afc77400b4742ad
jokingmoony <penubokim@gmail.com>
2024-03-31 18:41:58 +0900

    Add HostProperties
    Let's set cloud domains in cloud profile.

--- api-signaling/src/main/kotlin/org/example/api_signaling/App.kt
+++ api-signaling/src/main/kotlin/org/example/api_signaling/App.kt
@@ -1,9 +1,12 @@
 package org.example.api_signaling
 
+import org.example.api_signaling.properties.HostProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(HostProperties::class)
 class ApiSignalingApplication
 
 fun main(args: Array<String>) {
