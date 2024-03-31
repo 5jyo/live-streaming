43230f65f11330d15f8127e7b9d1a02acd0bc26f
jokingmoony <penubokim@gmail.com>
2024-03-31 18:41:17 +0900

    Remove open in Spring related classes

--- api-signaling/src/main/kotlin/org/example/api_signaling/App.kt
+++ api-signaling/src/main/kotlin/org/example/api_signaling/App.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class ApiSignalingApplication
+class ApiSignalingApplication
 
 fun main(args: Array<String>) {
     runApplication<ApiSignalingApplication>(*args)
