580b9ef8f63afd2d014de5760cbc8f97b0f56d84
jokingmoony <penubokim@gmail.com>
2024-04-07 17:09:34 +0900

    Add interactive streaming list for client

--- api-signaling/src/main/kotlin/org/example/api_signaling/controller/WebAppController.kt
+++ api-signaling/src/main/kotlin/org/example/api_signaling/controller/WebAppController.kt
@@ -4,19 +4,27 @@ import org.example.api_signaling.properties.HostProperties
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class MainController (private val hostProperties: HostProperties) {
+class WebAppController (private val hostProperties: HostProperties) {
 
     @GetMapping("/streamer")
     fun streamer(model: Model): String {
         model.addAttribute("host", hostProperties)
-        return "streamer"
+        return "streamer/main_view"
     }
 
     @GetMapping("/client")
     fun client(model: Model): String {
         model.addAttribute("host", hostProperties)
-        return "client"
+        return "client/main_view"
+    }
+
+    @GetMapping("/client/{roomId}")
+    fun watchingPage(@PathVariable roomId: String, model: Model): String {
+        model.addAttribute("host", hostProperties)
+        model.addAttribute("roomId", roomId)
+        return "client/watch_view";
     }
 }
