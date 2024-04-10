8406d0ba02ad26a252f0ae9dfb2600c96da92ff6
jokingmoony <penubokim@gmail.com>
2024-04-10 21:28:41 +0900

    Add docker-compose to run in local easily

--- api-signaling/src/main/kotlin/org/example/api_signaling/properties/HostProperties.kt
+++ api-signaling/src/main/kotlin/org/example/api_signaling/properties/HostProperties.kt
@@ -5,5 +5,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix="host")
 data class HostProperties(
     var meta: String = "",
+    var docker: Docker? = null,
     var signaling: String = "",
+)
+
+// Host properties for docker
+data class Docker(
+    var meta: String? = null
 )
